ed4031b835ca7887666d1a03fee25e53b4fb5bf4
Ishvini Anuraj <78601332+Ishvini29@users.noreply.github.com>
2023-03-19 00:30:48 +0530

    employee

--- src/pages/chapter/EnrollRequestSupervisor.js
+++ src/pages/chapter/EnrollRequestSupervisor.js
@@ -1,35 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../../components/NavBar";
+import axios from "axios";
 import employees from "../../data/Employee.json";
+import { Link } from "react-router-dom";
 
 const EnrollRequestSupervisor = () => {
-  const [buttonStates, setButtonStates] = useState(
-    employees.map(() => ({ accept: true, decline: true }))
-  );
-  const [acceptedRows, setAcceptedRows] = useState([]);
 
-  const handleAccept = (index) => {
-    const confirmed = window.confirm("Are you sure you want to accept this request?");
-    if (confirmed) {
-      const newButtonStates = [...buttonStates];
-      newButtonStates[index] = { accept: false, decline: false };
-      setButtonStates(newButtonStates);
-      setAcceptedRows([...acceptedRows, index]);
-    }
-  };
 
-  const handleDecline = (index) => {
-    const confirmed = window.confirm("Are you sure you want to decline this request?");
-    if (confirmed) {
-      const newButtonStates = [...buttonStates];
-      newButtonStates[index].accept = true;
-      newButtonStates[index].decline = false;
-      setButtonStates(newButtonStates);
-      setAcceptedRows([...acceptedRows, index]);
-    }
-  };
+  const [chapters, setChapter] = useState([]);
 
-  const isRowAccepted = (index) => acceptedRows.includes(index);
+  useEffect(() => {
+    axios.get("http://localhost:1337/chapters/showAllChapters")
+      .then(function (response) {
+        const filteredChapters = response.data.filter(chapter => chapter.depID !== null);
+        setChapter(filteredChapters);
+
+      });
+  }, []);
+
+  // const handleAccept = (index) => {
+  //   const confirmed = window.confirm("Are you sure you want to accept this request?");
+  //   if (confirmed) {
+  //     const newButtonStates = [...buttonStates];
+  //     newButtonStates[index] = { accept: false, decline: false };
+  //     setButtonStates(newButtonStates);
+  //     setAcceptedRows([...acceptedRows, index]);
+  //   }
+  // };
+
+  // const handleDecline = (index) => {
+  //   const confirmed = window.confirm("Are you sure you want to decline this request?");
+  //   if (confirmed) {
+  //     const newButtonStates = [...buttonStates];
+  //     newButtonStates[index].accept = true;
+  //     newButtonStates[index].decline = false;
+  //     setButtonStates(newButtonStates);
+  //     setAcceptedRows([...acceptedRows, index]);
+  //   }
+  // };
+
+  // const isRowAccepted = (index) => acceptedRows.includes(index);
 
   return (
     <React.Fragment>
@@ -51,22 +61,20 @@ const EnrollRequestSupervisor = () => {
             </tr>
           </thead>
           <tbody>
-            {employees.map((item, index) => {
-              if (isRowAccepted(index)) {
-                return null; // hide the row
-              }
+            {chapters.map((item) => {
+
               return (
-                <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.name}</td>
-                  <td>{item.jobtitle}</td>
-                  <td>{item.chapter}</td>
+                <tr key={item._id}>
+                  <td>{item.requested}</td>
+                  <td>name</td>
+                  <td>title</td>
+                  <td>{item.chaptername}</td>
                   <td>
                     <button
                       type="button"
                       className="btn btn-success form-control"
-                      disabled={!buttonStates[index].accept}
-                      onClick={() => handleAccept(index)}
+                    // disabled={!buttonStates[index].accept}
+                    // onClick={() => handleAccept(index)}
                     >
                       Accept
                     </button>
@@ -75,8 +83,8 @@ const EnrollRequestSupervisor = () => {
                     <button
                       type="button"
                       className="btn btn-danger form-control"
-                      disabled={!buttonStates[index].decline}
-                      onClick={() => handleDecline(index)}
+                    // disabled={!buttonStates[index].decline}
+                    // onClick={() => handleDecline(index)}
                     >
                       Decline
                     </button>
