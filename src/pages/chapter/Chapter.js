d05526176e15a433898c575a26f3326ccf7dca29
Raguraj Sivanantham <99555816+sragu2000@users.noreply.github.com>
2023-03-29 01:28:02 +0530

    unwanted imports

--- src/pages/chapter/Chapter.js
+++ src/pages/chapter/Chapter.js
@@ -1,10 +1,8 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
-import users from "../../data/Users.json";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import RedirectIfUserDontHavePermission from "../../utils/RedirectIfUserDontHavePermission";
 const Chapter = () => {
   const [chapters, setChapter] = useState([]);
 
