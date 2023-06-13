40a7934df096ed3ebde214ba03f183012fc4cb8a
sagini18 <saginisaju@gmail.com>
2023-06-13 18:26:52 +0530

    error fixed

--- src/routes/ChapterRoutes.js
+++ src/routes/ChapterRoutes.js
@@ -13,83 +13,88 @@ import PermanentDeleteChapter from "../pages/chapter/PermanentDeleteChapter";
 import ViewChapter from "../pages/chapter/ViewChapter";
 import DeleteChapterPermanent from "../pages/chapter/DeleteChapterPermanent";
 import Content from "../pages/Chapter page/Content";
-import EditTask from "../pages/Chapter page/EditTask";
+// import EditTask from "../pages/Chapter page/EditTask";
 
 import { userRoles as ur } from "../data/userRole";
 
 export const chapter_routes = [
-    {
-        path: "/chapter",
-        ele: <Chapter />,
-        availability: [ur.superAdmin]
-    },
-    {
-        path: "/newchap",
-        ele: <AddChapter />,
-        availability: [ur.superAdmin]
-    },
-    {
-        path: "/editchap/:id/:name",
-        ele: <EditChapter />,
-        availability: [ur.systemAdmin]
-    },
-    {
-        path: "/deletechap/:id",
-        ele: <DeleteChapter />,
-        availability: [ur.systemAdmin]
-    },
-    {
-        path: "/permanentdeletechapter",
-        ele: <PermanentDeleteChapter />,
-        availability: [ur.superAdmin]
-    },
-    {
-        path: "/depchapter",
-        ele: <DepartmentChapter />,
-        availability: [ur.systemAdmin]
-    },
-    {
-        path: "/viewchapter",
-        ele: <ViewChapter />,
-        availability: [ur.superAdmin, ur.systemAdmin, ur.supervisor, ur.contentCreator]
-    },
-    {
-        path: "/allocatechapter",
-        ele: <AllocateChapter />,
-        availability: [ur.systemAdmin]
-    },
-    {
-        path: "/editallocatechapter/:id/:name",
-        ele: <EditAllocate />,
-        availability: [ur.systemAdmin]
-    },
-    {
-        path: "/enrollrequestsupervisor",
-        ele: <EnrollRequestSupervisor />,
-        availability: [ur.supervisor]
-    },
-    {
-        path: "/enrollrequestemployee",
-        ele: <EnrollRequestEmployee />,
-        availability: [ur.hiredEmployee]
-    },
-    {
-        path: "/newdepchap",
-        ele: <DepartmentAddChapter />,
-        availability: [ur.systemAdmin]
-    },
-    {
-        path: "/deletechapper/:id",
-        ele: <DeleteChapterPermanent />,
-        availability: [ur.superAdmin]
-    },
-    {
-        path: "/chapterPage",
-        ele: <Content />,
-        availability:[ur.superAdmin]
-    },
-    {
-        path: "/edit/:id",
-        ele: <EditTask />
-    },
-]
+  {
+    path: "/chapter",
+    ele: <Chapter />,
+    availability: [ur.superAdmin],
+  },
+  {
+    path: "/newchap",
+    ele: <AddChapter />,
+    availability: [ur.superAdmin],
+  },
+  {
+    path: "/editchap/:id/:name",
+    ele: <EditChapter />,
+    availability: [ur.systemAdmin],
+  },
+  {
+    path: "/deletechap/:id",
+    ele: <DeleteChapter />,
+    availability: [ur.systemAdmin],
+  },
+  {
+    path: "/permanentdeletechapter",
+    ele: <PermanentDeleteChapter />,
+    availability: [ur.superAdmin],
+  },
+  {
+    path: "/depchapter",
+    ele: <DepartmentChapter />,
+    availability: [ur.systemAdmin],
+  },
+  {
+    path: "/viewchapter",
+    ele: <ViewChapter />,
+    availability: [
+      ur.superAdmin,
+      ur.systemAdmin,
+      ur.supervisor,
+      ur.contentCreator,
+    ],
+  },
+  {
+    path: "/allocatechapter",
+    ele: <AllocateChapter />,
+    availability: [ur.systemAdmin],
+  },
+  {
+    path: "/editallocatechapter/:id/:name",
+    ele: <EditAllocate />,
+    availability: [ur.systemAdmin],
+  },
+  {
+    path: "/enrollrequestsupervisor",
+    ele: <EnrollRequestSupervisor />,
+    availability: [ur.supervisor],
+  },
+  {
+    path: "/enrollrequestemployee",
+    ele: <EnrollRequestEmployee />,
+    availability: [ur.hiredEmployee],
+  },
+  {
+    path: "/newdepchap",
+    ele: <DepartmentAddChapter />,
+    availability: [ur.systemAdmin],
+  },
+  {
+    path: "/deletechapper/:id",
+    ele: <DeleteChapterPermanent />,
+    availability: [ur.superAdmin],
+  },
+  {
+    path: "/chapterPage",
+    ele: <Content />,
+    availability: [ur.superAdmin],
+  },
+  //   {
+  //     path: "/edit/:id",
+  //     ele: <EditTask />,
+  //   },
+];
--- src/routes/Editlogs.js
+++ src/routes/Editlogs.js
@@ -1,29 +1,28 @@
 import UnitHistory from "../pages/Chapter page/UnitHistory";
 import KTHistory from "../pages/Unit page/KTHistory";
 import ArticleHistory from "../pages/article page/ArticleHistory";
-import QuestionEditHistory from "../pages/Quiz/QuestionEditHistory";
+import QuestionEditHistory from "../pages/quiz/QuestionEditHistory";
 import EditHistory from "../pages/EditHistory";
 
 export const editlogs_routes = [
-    {
-        path: "/editunits",
-        ele: <UnitHistory/>
-    },
-    {
-        path: "/editkts",
-        ele: <KTHistory/>
-    },
-    {
-        path: "/editarticles",
-        ele: <ArticleHistory/>
-    },
-    {
-        path: "/editquestions",
-        ele: <QuestionEditHistory/>
-    },
-    {
-        path: "/edithistory",
-        ele: <EditHistory/>
-    }
-     
-]
+  {
+    path: "/editunits",
+    ele: <UnitHistory />,
+  },
+  {
+    path: "/editkts",
+    ele: <KTHistory />,
+  },
+  {
+    path: "/editarticles",
+    ele: <ArticleHistory />,
+  },
+  {
+    path: "/editquestions",
+    ele: <QuestionEditHistory />,
+  },
+  {
+    path: "/edithistory",
+    ele: <EditHistory />,
+  },
+];
