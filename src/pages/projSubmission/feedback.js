5cf2dbc19b77c0267bb0d2a6cead76d3533db5a6
sragu2000 <ragurajsivanantham@gmail.com>
2023-06-18 19:17:02 +0530

    sagini change

--- src/pages/projSubmission/feedback.js
+++ src/pages/projSubmission/feedback.js
@@ -6,15 +6,20 @@ import axios from "axios";
 const Feedback = () => {
   const API_BASE = "http://localhost:1337";
   const [employee, setEmployee] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const currentUser = jwt_decode(
     JSON?.parse(localStorage?.getItem("user"))?.token
   )?.userData?._id;
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get(API_BASE + "/getFeedback/" + currentUser)
-      .then((res) => setEmployee(res?.data))
+      .then((res) => {
+        setEmployee(res?.data)
+        setLoading(false);
+      })
       .catch((error) => {
         if (error?.response && error?.response?.status === 404) {
           // Handle "User not found" error
@@ -38,49 +43,55 @@ const Feedback = () => {
 
   return (
     <div>
-      {employee?.show ? ( // Checking if the employee data contains true in "show" field
-        <div className="container feedback shadow mt-5">
-          <h3 className="feedback-title py-3">Project Submission Feedback</h3>
-          <div className="feedback-body">
-            <>
-              <span className="me-1">Project Name</span>
-              <span>{employee?.projectName}</span>
-            </>
-            <>
-              <span className="">Score</span>
-              <span className="">{employee?.score} / 100</span>
-            </>
-            <>
-              <span className="">Feedback</span>
-              <span>{employee?.feedback}</span>
-            </>
-            <>
-              <span className="">Evaluated Time</span>
-              <span>{employee?.gradedOn}</span>
-            </>
-            <>
-              <span className="">Graded Supervisor</span>
-              <span className="d-flex">
-                <img
-                  className="img-fluid rounded-circle supervisor-avatar"
-                  src={employee?.supervisorImage}
-                  alt={employee?.gradedBy}
-                />
-                <span>{employee?.gradedBy}</span>
-              </span>
-            </>
-          </div>
-        </div>
-      ) : (
-        <div
-          className="shadow text-center bg-dark text-light"
-          width="90px"
-          height="90px"
-          style={{ margin: "15%", padding: "20px" }}
-        >
-          <h4>You are not allowed to view the results</h4>
-        </div>
-      )}
+      {
+        (loading)
+          ? <center><div className="spinner-grow mt-3" role="status"></div></center>
+          :
+          employee?.show ? ( // Checking if the employee data contains true in "show" field
+            <div className="container feedback shadow mt-5">
+              <h3 className="feedback-title py-3">Project Submission Feedback</h3>
+              <div className="feedback-body">
+                <>
+                  <span className="me-1">Project Name</span>
+                  <span>{employee?.projectName}</span>
+                </>
+                <>
+                  <span className="">Score</span>
+                  <span className="">{employee?.score} / 100</span>
+                </>
+                <>
+                  <span className="">Feedback</span>
+                  <span>{employee?.feedback}</span>
+                </>
+                <>
+                  <span className="">Evaluated Time</span>
+                  <span>{employee?.gradedOn}</span>
+                </>
+                <>
+                  <span className="">Graded Supervisor</span>
+                  <span className="d-flex">
+                    <img
+                      className="img-fluid rounded-circle supervisor-avatar"
+                      src={employee?.supervisorImage}
+                      alt={employee?.gradedBy}
+                    />
+                    <span>{employee?.gradedBy}</span>
+                  </span>
+                </>
+              </div>
+            </div>
+          ) : (
+            <div
+              className="shadow text-center bg-dark text-light"
+              width="90px"
+              height="90px"
+              style={{ margin: "15%", padding: "20px" }}
+            >
+              <h4>You are not allowed to view the results</h4>
+            </div>
+          )
+
+      }
     </div>
   );
 };
--- src/routes/QuizRoutes.js
+++ src/routes/QuizRoutes.js
@@ -3,15 +3,18 @@ import Quizpage from "../pages/quiz/Quizpage";
 import QuizDisplay from "../pages/quiz/QuizDisplay";
 import Result from "../pages/quiz/result";
 import Review from "../pages/quiz/review";
+import { userRoles as ur } from "../data/userRole";
 
 export const quiz_routes = [
   {
     path: "/result",
     ele: <Result />,
+    availability:[ur.hiredEmployee]
   },
   {
     path: "/review",
     ele: <Review />,
+    availability:[ur.hiredEmployee, ur.supervisor]
   },
   {
     path: "/quiz/view",
